6889ada7f387c6b0fdafa77b5642d08b70685187
RudiFrost <rudifrost255@gmai.com>
2021-04-01 11:22:35 +0300

    requirements.txt

--- flask/main.py
+++ flask/main.py
@@ -182,9 +182,10 @@ def main():
     # для одного объекта
     api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
 
-    port = int(os.environ.get("PORT", 5000))
+
     # app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
